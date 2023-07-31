0075b89ef6f4903bb3de628b9a992f70d16c3ccd
PathumMalshika <135942146+PathumMalshika@users.noreply.github.com>
2023-07-31 23:43:13 +0530

    Add files via upload

---
+++ Labsheets(JAVA)/Labsheet 10/ArithmeticExceptionExe/src/main/java/com/mycompany/arithmeticexceptionexe/ArithmeticExceptionExe.java
@@ -0,0 +1,13 @@
+
+
+package com.mycompany.arithmeticexceptionexe;
+
+
+public class ArithmeticExceptionExe {
+
+    public static void main(String[] args) {
+        
+        ArithmeticExceptionHanding a1 = new ArithmeticExceptionHanding (5,0);
+        a1.divition();
+    }
+}
---
+++ Labsheets(JAVA)/Labsheet 10/ArithmeticExceptionExe/src/main/java/com/mycompany/arithmeticexceptionexe/ArithmeticExceptionHanding.java
@@ -0,0 +1,44 @@
+
+package com.mycompany.arithmeticexceptionexe;
+
+
+public class ArithmeticExceptionHanding {
+    
+    
+    private int a, b,z ;    
+     
+    public ArithmeticExceptionHanding(int a , int b) {
+    
+    
+     this.a = a;  
+     this.b = b;
+    
+    
+    }
+    
+     public void divition () {
+         
+     try {  
+      this.z = this.a/this.b;
+     } catch (e.getMessage()  e) {
+
+       System.out.println(e.getMessage());
+
+     
+     }
+     
+     
+   }
+
+
+
+
+
+        
+    
+  
+    
+    
+  
+    
+}
